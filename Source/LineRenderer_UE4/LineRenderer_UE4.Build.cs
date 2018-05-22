d2c6c910609ea76a4699b91a12b5375cb2ea5abc
Nick Fox-Gieg <nick@fox-gieg.com>
2018-05-22 00:10:48 -0400

    remove RuntimeMeshComponent

--- Source/LineRenderer_UE4/LineRenderer_UE4.Build.cs
+++ Source/LineRenderer_UE4/LineRenderer_UE4.Build.cs
@@ -8,7 +8,7 @@ public class LineRenderer_UE4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
